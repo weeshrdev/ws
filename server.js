fd557c3c2f61fb7d4fd9d2734b4cca60fea65fd7
Adeola Ephraim <62262920+codeDeeAi@users.noreply.github.com>
2023-10-20 15:36:08 +0100

    init

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const createLocalServer = require('http').createServer;
+const createHttpsServer = require('https').createServer;
+
+const app = express();
+const port = 3001;
+const host = '0.0.0.0';
+const node_env = 'production';
+
+app.use(express.json());
+
+const server = (node_env == 'production')
+    ? createHttpsServer({}, app)
+    : createLocalServer({}, app);
+
+server.listen(port, host, () => {
+    console.log(`⚡️[server]: Server is running on port:${port}`);
+});
+
+app.get('/', (req, res) => {
+    res.status(200).json({ message: 'Welcome to Weeshr app Server' });
+});
